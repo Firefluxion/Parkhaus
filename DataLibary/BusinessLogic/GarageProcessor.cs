35639f04447190c5b14099c01c5c49e0f6142b2e
Firefluxion <robin@gerwe.de>
2021-06-09 02:17:13 +0200

    Added Garages
    -Added GarageProcessor.cs
    -Added Migration Create Table Garages
    -Added GarageModel.cs

---
+++ DataLibary/BusinessLogic/GarageProcessor.cs
@@ -0,0 +1,43 @@
+﻿using DataLibary.DataAccess;
+using DataLibary.Models;
+
+namespace DataLibary.BusinessLogic
+{
+    public class GarageProcessor
+    {
+        private readonly MySqlDataAccess sqlDataAccess;
+
+        public GarageProcessor(MySqlDataAccess sqlDataAccess)
+        {
+            this.sqlDataAccess = sqlDataAccess;
+        }
+
+        public int CreateGarage(int id, string name, int capacity, int reservedForLongTerm, int shortTermAccessMinThreshold, int displayOccupiedThreshold)
+        {
+            GarageModel garage = new GarageModel {
+                ID = id,
+                Name = name,
+                Capacity = capacity,
+                ReservedForLongTerm = reservedForLongTerm,
+                ShortTermAccessMinThreshold = shortTermAccessMinThreshold,
+                DisplayOccupiedThreshold = displayOccupiedThreshold
+            };
+
+            string sql = @"insert into Garages (ID, Name, Capacity, ReservedForLongTerm, ShortTermAccessMinThreshold, DisplayOccupiedThreshold)
+                           values (@ID, @Name, @Capacity, @ReservedForLongTerm, @ShortTermAccessMinThreshold, @DisplayOccupiedThreshold)";
+
+            return sqlDataAccess.SaveData(sql, garage);
+        }
+
+        public GarageModel LoadGarageByName(string name)
+        {
+            GarageModel garage = new GarageModel { Name = name }
+            ;
+            string sql = @"select ID, Name, Capacity, ReservedForLongTerm, ShortTermAccessMinThreshold, DisplayOccupiedThreshold
+                           from Garages
+                           where Name = @Name;";
+
+            return sqlDataAccess.LoadData(sql, garage);
+        }
+    }
+}
