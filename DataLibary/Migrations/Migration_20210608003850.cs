c5502a5107495226be23181ce8f767fad479ecb6
Firefluxion <robin@gerwe.de>
2021-06-09 02:28:25 +0200

    Added Migration Insert Into Costs LongTerm Values

---
+++ DataLibary/Migrations/Migration_20210608003850.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace DataLibary.Migrations
+{
+    [Migration(20210608003850, "Insert Into Costs LongTerm Values")]
+    public class Migration_20210608003850 : Migration
+    {
+        public override void Down()
+        {
+            Delete.FromTable("Costs").Row(new { ID = 1 });
+        }
+
+        public override void Up()
+        {
+            Insert.IntoTable("Costs")
+                .Row(new {
+                    ID = 1,
+                    Name = "LongTerm",
+                    EuroPerHour = 0.69
+                });
+        }
+    }
+}
