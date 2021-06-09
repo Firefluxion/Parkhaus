55e2ec8602bf1ec07b27d59e62a88f37ce80bf7b
Firefluxion <robin@gerwe.de>
2021-06-09 09:39:14 +0200

    Added ParkTicketModel.cs

---
+++ DataLibary/Models/ParkTicketModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLibary.Models
+{
+    public class ParkTicketModel
+    {
+        public int ID { get; set; }
+        public string LicensePlate { get; set; }
+        public bool LongTerm { get; set; }
+    }
+}
