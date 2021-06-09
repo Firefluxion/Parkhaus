c999e92a79a30237a0c0ae25502e1eb3b8c1752c
Firefluxion <robin@gerwe.de>
2021-06-09 02:27:16 +0200

    Added Migration Create Table Costs

---
+++ DataLibary/Migrations/Migration_20210608001310.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace DataLibary.Migrations
+{
+    [Migration(20210608001310, "Create Table Costs")]
+    public class Migration_20210608001310 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Costs");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Costs")
+                .WithColumn("ID").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("EuroPerHour").AsDecimal().NotNullable();
+        }
+    }
+}
