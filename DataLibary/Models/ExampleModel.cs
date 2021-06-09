018333bd225926d4e4650f3d7efbc18cd4c8473d
Firefluxion <robin@gerwe.de>
2021-06-09 09:03:06 +0200

    Removed ExampleModel
    not needed

--- DataLibary/Models/ExampleModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataLibary.Models
-{
-    public class ExampleModel
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
